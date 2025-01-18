c3a2252d42e58eadf998faa65c8c52b9b408be4a
yasaswinigongati <gongatiyasaswini12@gmail.com>
2025-01-18 10:50:10 +0530

    Project completed

---
+++ backend/src/seeds/user.seed.js
@@ -0,0 +1,41 @@
+import { config } from "dotenv";
+import { connectDB } from "../lib/db.js";
+import User from "../models/user.model.js";
+
+config();
+
+const seedUsers = [
+  // Female Users
+  
+  {
+    email: "olivia.miller@example.com",
+    fullName: "Olivia Miller",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  
+
+  // Male Users
+  
+  {
+    email: "lucas.moore@example.com",
+    fullName: "Lucas Moore",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/4.jpg",
+  },
+  
+];
+
+const seedDatabase = async () => {
+  try {
+    await connectDB();
+
+    await User.insertMany(seedUsers);
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+
+// Call the function
+seedDatabase();
